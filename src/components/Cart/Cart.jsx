a17a63c29af71f006dd88861d306a9bfbffa7ead
konivan <konivan08@gmail.com>
2022-09-17 19:31:23 +0300

    refactor

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import CartFooter from '../CartFooter.jsx/CartFooter';
 import CartHeader from '../CartHeader/CartHeader';
 import Product from '../Product/Product';
+import data from '../../data';
+import { useState } from 'react';
 
 const Cart = () => {
+
+  const [cart, setCart] = useState(data);
+
   return (
     <section className="cart">
         <CartHeader />
-        <Product />
+        {cart.map((item) =>
+        <Product item={item} key={item.id}/>
+        )}
         <CartFooter />
     </section>
   );
